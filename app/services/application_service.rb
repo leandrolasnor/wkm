1582e76796e6d1d54bbdfba5df690d7fd5a53bae
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-20 19:06:44 +0000

    feat: application service class

---
+++ app/services/application_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ApplicationService
+  extend Dry::Initializer
+
+  def self.call(args)
+    validator = self::Schema.call(args.to_h)
+    if validator.success?
+      content, status, serializer = new(args).call
+      return { content: content, status: status, serializer: serializer }
+    end
+
+    { content: validator.errors, status: :unprocessable_entity }
+  end
+end
