cc7a59bfa93ef7e7bc82c9fd37d72e5897218434
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-20 19:08:19 +0000

    chore: alias for debbug command and rubocop

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
+
+alias bug debugger
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
