396e6b019934496663f85b517f69a547ef1a20e5
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-20 22:08:09 +0000

    test: teste case for delete :fire

--- spec/requests/employees_controller_spec.rb
+++ spec/requests/employees_controller_spec.rb
@@ -3,15 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe EmployeesController do
+  before do
+    Timecop.freeze('2023-07-01 09:00:00')
+  end
+
+  after do
+    Timecop.return
+  end
+
   describe "POST /hire" do
-    before do
-      Timecop.freeze('2023-07-01 09:00:00')
-    end
-
-    after do
-      Timecop.return
-    end
-
     context "when hire a employee" do
       context "on success" do
         let(:params) { { name: 'Valdez', position: 'Analyst' } }
@@ -24,7 +24,7 @@ RSpec.describe EmployeesController do
         end
 
         before do
-          post(employees_hire_path, params: params, as: :json)
+          post(hire_employee_path, params: params, as: :json)
         end
 
         it "must be able to hire a employee" do
@@ -33,7 +33,9 @@ RSpec.describe EmployeesController do
         end
       end
     end
+  end
 
+  describe "PATCH /promotion" do
     context "when promotion a employee" do
       context "on success" do
         let(:params) { { id: employee.id, position: 'Chair Man' } }
@@ -54,12 +56,39 @@ RSpec.describe EmployeesController do
         end
 
         before do
-          patch(employees_promotion_path, params: params, as: :json)
+          patch(promotion_employee_path, params: params, as: :json)
         end
 
         it "must be able to advanced a employee to Chair Man" do
           expect(response).to be_successful
           expect(json_body).to eq(expected_json_body)
+        end
+      end
+    end
+  end
+
+  describe "DELETE /fire" do
+    context 'when fire a employee' do
+      context 'on success' do
+        let(:params) { { id: employee.id } }
+        let(:paranoia_employee) { Employee.only_deleted.find(employee.id) }
+        let(:employee) { create(:employee, :analyst, name: 'Arthur') }
+        let(:expected_json_body) do
+          {
+            name: 'Arthur',
+            position: 'Analyst',
+            hire_date: '2023-07-01'
+          }
+        end
+
+        before do
+          delete(fire_employee_path, params: params)
+        end
+
+        it 'must be able to soft delete the employee' do
+          expect(response).to be_successful
+          expect(json_body).to eq(expected_json_body)
+          expect(paranoia_employee).not_to be_nil
         end
       end
     end
