1eaf47a7fb2b6bb36d4fc56b620a61ede2415bc3
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-20 19:05:33 +0000

    feat(models): vacation and employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Employee < ApplicationRecord
+  acts_as_paranoid
+
   has_many :vacations, dependent: :destroy
 end
--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Vacation < ApplicationRecord
+  acts_as_paranoid
+
   belongs_to :employee
 end
