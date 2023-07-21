b6c67081a788833d80fd85f3567e069924091be9
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-21 14:47:44 +0000

    feat(service): schedule vacation

---
+++ app/services/schedule_vacation_srvc.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ScheduleVacationSrvc < ApplicationService
+  param :params
+
+  option :serializer, default: -> { VacationSerializer }
+  option :repository, default: -> { Scheduling::Vacation.new(params) }
+  Schema = Scheduling::Vacation::Contract.new
+
+  def call
+    scheduled = repository.schedule
+
+    [scheduled, :created, serializer]
+  end
+end
