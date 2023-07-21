98a106e9657135e564f4dcedc056b2e516b02c70
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-21 19:38:17 +0000

    feat(service): list employee

---
+++ app/services/list_employee_srvc.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ListEmployeeSrvc < ApplicationService
+  param :params
+
+  option :serializer, default: -> { ListEmployeeSerializer }
+  option :repository, default: -> { Listing::Employees.new(params) }
+  Contract = Listing::Employee::Contract.new
+
+  def call
+    list = repository.list
+
+    [list, :ok, serializer]
+  end
+end
