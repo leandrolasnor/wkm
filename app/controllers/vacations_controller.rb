bcc17b02117f96e74834f7fa42eec1fa65269448
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-21 14:46:03 +0000

    feat(controller): post vacation/schedule

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class VacationsController < ApiController
+  def schedule
+    resolve(**ScheduleVacationSrvc.call(schedule_params))
+  end
+
+  private
+
+  def vacation_params
+    params.fetch(:vacation, {})
+  end
+
+  def schedule_params
+    vacation_params.permit(:start_date, :end_date, :employee_id)
+  end
 end
