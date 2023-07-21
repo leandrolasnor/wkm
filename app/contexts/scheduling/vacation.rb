77db25b53dbb587803c820d3b52e335bff6b3a41
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-21 22:23:01 +0000

    feat(context): law MCMLXXVII::Abr15Art130N1535

--- app/contexts/scheduling/vacation.rb
+++ app/contexts/scheduling/vacation.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true 
+# frozen_string_literal: true
 
 class Scheduling::Vacation
   extend Dry::Initializer
@@ -6,8 +6,16 @@ class Scheduling::Vacation
   param :params
 
   option :model, default: -> { Vacation }
+  option :employment_laws, default: -> {
+    [
+      Laws::MCMLXXVII::Abr15Art130N1535.new.call(params.to_h)
+    ]
+  }
+  option :preventions, default: -> { employment_laws.select(&:failure?) }
 
   def schedule
-    model.create(params)
+    return model.create(params) unless preventions.any?
+
+    raise StandardError.new(preventions.flatten.map { _1.errors.to_h }.to_json)
   end
 end
