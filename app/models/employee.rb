9400b54ee6c028b222014a5cb39d419755c96fab
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-22 18:41:11 +0000

    feat(model): using calculating days context

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,13 @@ class Employee < ApplicationRecord
   acts_as_paranoid
 
   has_many :vacations, dependent: :destroy
+
+  delegate :vacation_days_available, to: :calculating_days
+  delegate :enjoyed, to: :calculating_days
+
+  private
+
+  def calculating_days
+    @calculating_days ||= Calculating::Days::Employee.new(self)
+  end
 end
--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -4,4 +4,12 @@ class Vacation < ApplicationRecord
   acts_as_paranoid
 
   belongs_to :employee
+
+  delegate :days, to: :calculating_days
+
+  private
+
+  def calculating_days
+    @calculating_days ||= Calculating::Days::Vacation.new(self)
+  end
 end
