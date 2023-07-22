6e22817b48d1b6ded3a395278ad424d415a22444
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-22 18:38:13 +0000

    feat(contract):avoid fire employee on enjoying

---
+++ app/contracts/firing/employee/fireable/contract.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Firing::Employee::Fireable::Contract < Dry::Validation::Contract
+  config.messages.backend = :i18n
+
+  params do
+    required(:employee_id).filled(:integer)
+  end
+
+  rule(:employee_id) do
+    employee = Employee.find(value)
+    fireable = !employee.vacations.exists?(['? between start_date and end_date', Time.zone.now])
+
+    key.failure(:fireable) unless fireable
+  end
+end
