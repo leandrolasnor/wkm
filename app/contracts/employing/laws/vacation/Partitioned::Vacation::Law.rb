1a4e3ef09705a7e5d5d8ae9019587a48f03697b0
Leandro Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-23 12:01:36 -0300

    feat(contract): Partitioned Vacation Law

---
+++ app/contracts/employing/laws/vacation/Partitioned::Vacation::Law.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Employing::Laws::Partitioned::Vacation::Law < Employing::Laws::Vacation::Partitioned::Macros
+  params do
+    required(:employee_id).value(:integer)
+    required(:partitions).array(:hash) do
+      required(:start_date).value(:string)
+      required(:end_date).value(:string)
+    end
+  end
+
+  rule(:employee_id, :partitions).validate(:availability)
+  rule(:employee_id, :partitions).validate(:overlap)
+  rule(:employee_id, :partitions).validate(:partitioned)
+end
