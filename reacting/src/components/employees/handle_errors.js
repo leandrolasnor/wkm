9d475ea544d4ca0ec107669a10480e9fb12e5b11
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-25 15:51:02 +0000

    feat: improving error handler

--- reacting/src/components/employees/handle_errors.js
+++ reacting/src/components/employees/handle_errors.js
@@ -3,13 +3,17 @@ import { toastr } from "react-redux-toastr";
 var _ = require('lodash')
 
 const handle_errors = e => {
-  if(e.response.data){
+  if(_.get(e, 'response.data', false)){
     Object.entries(e.response.data).forEach(error => {
       const [key, value] = error;
       toastr.error(_.capitalize(key), value.join(' '));
     });
-  }else{
+  }else if(_.get(e, 'response', false)){
     toastr.error(String(e.response.status), e.response.statusText);
+  }else if(_.get(e, 'message', false) === 'Network Error'){
+    toastr.error('API', e.message);
+  }else{
+    toastr.error('Error', _.get(e, 'message', 'unknown error'));
   }
 }
 
