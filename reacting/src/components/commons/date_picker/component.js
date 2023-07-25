e9d85b6602904e267bde6aaa8672473923dbfd67
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-07-25 15:45:41 +0000

    chore: removing some variables

--- reacting/src/components/commons/date_picker/component.js
+++ reacting/src/components/commons/date_picker/component.js
@@ -1,7 +1,7 @@
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css';
 
-const renderDatePicker = ({ input, label, type, className, placeholder, autocomplete, minDate, dateFormat, selected, meta: { touched, error } }) => (
+const renderDatePicker = ({ input, type, placeholder, autocomplete, minDate, dateFormat, selected }) => (
   <DatePicker {...input} autoComplete={autocomplete} minDate={minDate} dateFormat={dateFormat} type={type} selected={selected} placeholderText={placeholder} />
 )
 
